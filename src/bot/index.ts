b054bd8eb328b243c1c3f8f52e59e782e80cf470
Include caleb.fun <70612830+oasisMystre@users.noreply.github.com>
2024-04-14 02:45:41 +0100

    feat: fix bot bugs and improve

--- src/bot/index.ts
+++ src/bot/index.ts
@@ -2,7 +2,7 @@ import { Markup, type Telegraf } from "telegraf";
 import type { TelegramContext } from "../context";
 import { buildPathWithQuery } from "../utils/url";
 
-export const echo = async function (ctx: TelegramContext) {
+const echo = async function (ctx: TelegramContext) {
   const user = ctx.from!;
   const url = buildPathWithQuery(process.env.APP_URL!, {
     id: user.id,
@@ -18,6 +18,18 @@ export const echo = async function (ctx: TelegramContext) {
   await ctx.replyWithMarkdownV2(
     "*Let's get started* \n \n Please tap the button below to launch MuseAI",
     Markup.inlineKeyboard([Markup.button.webApp("Launch App", url)])
+  );
+};
+
+const onHelp = function (ctx: TelegramContext) {
+  return ctx.replyWithMarkdownV2(
+    "*MuseAI* \n\n MuseAI is a generative AI bot that helps you create music and instrumentals. \n\n *How to use?* \n - Start up the bot using /start command \n - Launch the telegram webapp using the inline button \n"
+  );
+};
+
+const onSocials = function (ctx: TelegramContext) {
+  return ctx.replyWithMarkdownV2(
+    "*Join our socials* \n [Telegram]() \n [x]() \n\n Or visit our website [MuseAI]()"
   );
 };
 
@@ -41,10 +53,10 @@ export default function registerBot(bot: Telegraf<TelegramContext>) {
 
   bot.command("message", async (ctx) => {
     const message = ctx.message;
-    
+
     if ("text" in message) {
-      if (message.text === "/socials") return;
-      if (message.text === "/help") return;
+      if (message.text === "/socials") return onHelp(ctx);
+      if (message.text === "/help") return onSocials(ctx);
     }
 
     await echo(ctx);
