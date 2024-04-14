79dfe3fbb7e10a771b69fa6066e40cab10b608e5
Include caleb.fun <70612830+oasisMystre@users.noreply.github.com>
2024-04-14 19:03:42 +0100

    Update file.ts

--- suno-app/src/lib/utils/file.ts
+++ suno-app/src/lib/utils/file.ts
@@ -1,4 +1,4 @@
 export const urlToFile = async function (url: string, name: string) {
   const response = await fetch(url);
-  return new File([await response.blob()], name);
-};
+  return new File([await response.blob()], name, {type: "audio/mpeg" });
+}
