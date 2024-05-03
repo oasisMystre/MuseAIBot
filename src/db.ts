fd2868296907786ad86676cafb350d35e8ad864f
Include caleb.fun <70612830+oasisMystre@users.noreply.github.com>
2024-05-03 08:33:23 +0100

    feat: update pg connection

--- src/db.ts
+++ src/db.ts
@@ -2,11 +2,10 @@ import postgres from "postgres";
 import { drizzle } from "drizzle-orm/postgres-js";
 
 import schema from "./schema";
-import { dbConfig } from "./config";
 
 export function createDB(connection: ReturnType<typeof postgres>) {
   const db = drizzle(connection, { schema });
   return db;
 }
 
-export const db = createDB(postgres(dbConfig));
+export const db = createDB(postgres(process.env.DATABASE_URL!));
