3c00b29b7b30150743f2f9a40a989d519d5b02a6
Include caleb.fun <70612830+oasisMystre@users.noreply.github.com>
2024-05-03 18:52:04 +0100

    feat: update playback

--- suno-app/src/components/MusicDetailDialog.tsx
+++ suno-app/src/components/MusicDetailDialog.tsx
@@ -1,8 +1,9 @@
 import moment from "moment";
-import { MdClose, MdPerson, MdPlayArrow } from "react-icons/md";
+import { MdClose, MdPause, MdPerson, MdPlayArrow } from "react-icons/md";
 
 import usePlayer from "../composables/usePlayer";
 import { LibraryAndAudioInfo } from "../lib/api/models";
+import { useAuth } from "../composables/useAuth";
 
 type MusicDetailDialogProps = {
   library: LibraryAndAudioInfo;
@@ -10,10 +11,13 @@ type MusicDetailDialogProps = {
 };
 
 export default function MusicDetailDialog({
-  library: { library, audioInfo },
+  library,
   onClose,
 }: MusicDetailDialogProps) {
-  const { isPlaying, toggle, playAudio } = usePlayer();
+  const { user } = useAuth();
+  const { isPlaying, toggle, playAudio, currentPlaying } = usePlayer();
+  const isCurrentPlaying =
+    currentPlaying?.audioInfo.id === library.audioInfo.id;
 
   return (
     <div className="fixed inset-0 bg-black/50 overflow-y-scroll z-10">
@@ -25,36 +29,45 @@ export default function MusicDetailDialog({
             </button>
           </div>
           <img
-            src={audioInfo.image_url}
-            alt={audioInfo.title}
+            src={library.audioInfo.image_url}
+            alt={library.audioInfo.title}
             className="self-center w-64 h-64 bg-white rounded-md"
           />
-          <h1 className="text-2xl font-bold">{audioInfo.title}</h1>
+          <h1 className="text-2xl font-bold">{library.audioInfo.title}</h1>
           <div className="flex flex-col space-y-2">
             <div className="flex items-center space-x-2">
               <MdPerson />
-              <span className="hidden">typenonull</span>
+              <span>{user.username ?? user.firstName}</span>
             </div>
-            <p>{audioInfo.tags}</p>
+            <p>{library.audioInfo.tags}</p>
             <small className="opacity-80">
-              {moment(audioInfo.created_at).format("MMMM Do YYYY")}
+              {moment(library.audioInfo.created_at).format("MMMM Do YYYY")}
             </small>
           </div>
           <button
             className="self-start flex space-x-2 items-center bg-white text-black px-4 py-2 rounded-md"
             onClick={() => {
-              const libraryAndAudioInfo = { audioInfo, library };
-              playAudio(libraryAndAudioInfo);
+              if (isCurrentPlaying) toggle();
+              else playAudio(library);
             }}
           >
-            <MdPlayArrow className="text-2xl" />
-            <span>Play</span>
+            {isPlaying && isCurrentPlaying ? (
+              <>
+                <MdPause />
+                <span>Pause</span>
+              </>
+            ) : (
+              <>
+                <MdPlayArrow className="text-2xl" />
+                <span>Play</span>
+              </>
+            )}
           </button>
         </div>
         <div className="flex-1 flex flex-col space-y-4 px-8  opacity-80">
           <p className="font-medium">Lyrics</p>
           <div className="flex flex-col space-y-6 whitespace-pre-line prose">
-            {audioInfo.lyric.split(/(?=\[.*\])/i).map((lyric) => (
+            {library.audioInfo.lyric.split(/(?=\[.*\])/i).map((lyric) => (
               <div>{lyric}</div>
             ))}
           </div>
